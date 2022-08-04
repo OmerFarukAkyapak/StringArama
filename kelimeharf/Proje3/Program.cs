e0f92d0df360c3fb81878fb30db5a85d9f7b3f88
Faruk Akyapak <56655714+OmerFarukAkyapak@users.noreply.github.com>
2022-08-04 23:31:24 +0300

    kelimeharf
    girilen kelimede harf araması

---
+++ kelimeharf/Proje3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kelimenizi giriniz : ");
+            String x = Console.ReadLine();
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            dict = fonksiyon(x);
+            foreach (var dictitem in dict.Keys)
+            {
+                Console.WriteLine("{0} harfi {1} kez tekrar ediyor", dictitem, dict[dictitem]);
+                Console.ReadLine();
+            }
+
+        }
+        static Dictionary<char, int> fonksiyon(string j)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            foreach (char c in j)
+                if (!dict.ContainsKey(c))
+                    dict.Add(c, 1);
+                else
+                    dict[c] = dict[c] + 1;
+            return dict;
+            Console.ReadLine();
+        }
+    }
+}
